7bf6e5b30c8b0dc23df00e969f2f4832e28cd929
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-03-26 10:41:08 -0400

    Added Playlist functionality
    Added Playlist functionality.

--- Personal-Scripts/yt-dlp-search.sh
+++ Personal-Scripts/yt-dlp-search.sh
@@ -2,10 +2,20 @@
 #MattTheTekie's terminal based Youtube searching script. 
 #Use -s query, or run the script to query content.
 #Requires mpv, and yt-dlp.
-while getopts "s:" opt; do
+while getopts "p:s:" opt; do
   case $opt in
+    p)
+      playlist=$OPTARG
+      mpv "$playlist" --ytdl-format="bestvideo[height<=?720][fps<=?30]+bestaudio/best" --screen=1 --sid=1
+      ;;
     s)
       searchterm=$OPTARG
+          echo $@ >> 1.txt
+    sed 's/-s //' -i 1.txt
+    sed -n 1,1p 1.txt
+    export BWD="$(cat 1.txt)"
+    mpv ytdl://ytsearch:"$BWD" --ytdl-format="bestvideo[height<=?720][fps<=?30]+bestaudio/best" --screen=1 --sid=1
+    rm 1.txt
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -13,21 +23,8 @@ while getopts "s:" opt; do
       ;;
   esac
 done
-
-if [ -z "$searchterm" ]; then
+if [ -z "$searchterm" ] && [ -z "$playlist" ]; then
   echo -n "Enter Searchterm: "
   read searchterm
   mpv ytdl://ytsearch:"$searchterm" --ytdl-format="bestvideo[height<=?720][fps<=?30]+bestaudio/best" --screen=1 --sid=1
-  exit 1
-fi
-
-for user in "$@"
-do
-    echo $@ >> 1.txt
-    sed 's/-s //' -i 1.txt
-    sed -n 1,1p 1.txt
-    export BWD="$(cat 1.txt)"
-    mpv ytdl://ytsearch:"$BWD" --ytdl-format="bestvideo[height<=?720][fps<=?30]+bestaudio/best" --screen=1 --sid=1
-    rm 1.txt
-exit
-done
+  fi
