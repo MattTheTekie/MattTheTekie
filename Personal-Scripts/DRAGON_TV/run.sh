72a14a021f19125e615175385f1d4ce3e5759b4d
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-10 12:48:01 -0400

    Update run.sh

--- Personal-Scripts/DRAGON_TV/run.sh
+++ Personal-Scripts/DRAGON_TV/run.sh
@@ -3,7 +3,14 @@
 while true
 do
   # Run the script
-  ./randomize.sh
+# Read commands from file into an array
+readarray -t commands < final.sh
+
+# Get a random index from the commands array
+rand_index=$((RANDOM % ${#commands[@]}))
+
+# Run the randomly chosen command
+eval "${commands[$rand_index]}"
 
   # Check the exit status
   exit_status=$?
