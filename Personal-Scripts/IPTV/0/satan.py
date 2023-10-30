f2e7612320121da3437f2c3898aeaa7fcf87b053
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-10-30 17:29:15 -0400

    Update satan.py

--- Personal-Scripts/IPTV/0/satan.py
+++ Personal-Scripts/IPTV/0/satan.py
@@ -1,5 +1,5 @@
 # Open the M3U file and read its content
-with open('/home/runner/work/MattTheTekie/MattTheTekie/Personal-Scripts/IPTV/0/jp_org.m3u', 'r') as file:
+with open('jp_org.m3u', 'r') as file:
     lines = file.readlines()
 
 # Remove duplicates
@@ -11,5 +11,5 @@ for line in lines:
         lines_seen.add(line)
 
 # Write the non-duplicate content back to the file
-with open('/home/runner/work/MattTheTekie/MattTheTekie/Personal-Scripts/IPTV/0/jp_org.m3u', 'w') as file:
+with open('jp_org.m3u', 'w') as file:
     file.writelines(new_lines)
