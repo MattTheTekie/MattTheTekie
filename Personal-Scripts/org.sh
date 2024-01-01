4fe4a391091c7336168f6bc6583dfff48e921345
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2024-01-01 13:06:23 -0500

    Update org.sh

--- Personal-Scripts/org.sh
+++ Personal-Scripts/org.sh
@@ -2,16 +2,17 @@
 
 ORG_NAME="VENITHNET"
 README_FILE="README.md"
+GITHUB_TOKEN="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"
 
-# Get the list of repositories from the GitHub API
-repositories=$(curl -s "https://api.github.com/orgs/$ORG_NAME/repos?per_page=100" | jq -r '.[].contributors_url')
+# Get the list of repositories from the GitHub API with your GitHub Access Token
+repositories=$(curl -s -H "Authorization: bearer $GITHUB_TOKEN" "${{ secrets.GITHUB_TOKEN }}" | jq -r '.[].contributors_url')
 
 # Initialize an array to store unique contributors
 unique_contributors=()
 
 # Loop through each repository's contributors and add to the array
 for contributor_url in $repositories; do
-    repo_contributors=$(curl -s "$contributor_url" | jq -r '.[].login')
+    repo_contributors=$(curl -s -H "Authorization: bearer $GITHUB_TOKEN" "$contributor_url" | jq -r '.[].login')
     unique_contributors+=($repo_contributors)
 done
 
@@ -24,7 +25,10 @@ total_contributors=${#unique_contributors[@]}
 # Create the badge URL with the actual number of contributors
 badge_url="https://img.shields.io/badge/contributors-$total_contributors%20contributors-yellow.svg"
 
-# Replace existing contributors badge URL in README file
-sed -i "s|!\[Contributors\](.*)|![Contributors]($badge_url)|g" "$README_FILE"
+# Echo the badge URL
+echo "Badge URL: $badge_url"
+
+# Replace existing contributors badge URL in README file using sed
+sed -i "s|https://img.shields.io/github/contributors/$ORG_NAME/VENITH.NET|$badge_url|g" "$README_FILE"
 
 echo "Badge URL replaced in $README_FILE"
